58ada91257515474482d680498dc63b0caf174cc
Diogo <diogozang2@gmail.com>
2023-05-22 22:09:37 -0300

    fix: safe &apos; use
    safe &apos; use

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,14 +17,14 @@ export default function Home() {
       <Section title="A little beyond the code">
         <>
           <p>
-            With over 10 years of passion for language learning, I'm constantly
+            With over 10 years of passion for language learning, I&apos;m constantly
             seeking new ways to explore different cultures and communicate with
             people from around the world. Portuguese is my mother language, but,
-            currently, I'm fluent in English, have an advanced knowledge of
-            Spanish, and I'm studying Italian.
+            currently, I&apos;m fluent in English, have an advanced knowledge of
+            Spanish, and I&apos;m studying Italian.
           </p>
           <p>
-            Since 2017, I've had the opportunity to put into practice what I
+            Since 2017, I&apos;ve had the opportunity to put into practice what I
             love and work with people from various nationalities. This
             experience has allowed me to develop valuable interpersonal and
             intercultural skills, as well as learn to adapt my communication to
@@ -33,13 +33,13 @@ export default function Home() {
           <p>
             My commitment to creating quality digital solutions for a global
             audience is a natural extension of this passion. With my language
-            skills and knowledge, I'm capable of creating internationalized
+            skills and knowledge, I&apos;m capable of creating internationalized
             products that are accessible and understandable to people from
             different backgrounds.
           </p>
           <p>
-            If you're looking for a developer who can ensure your product is
-            effective in a large part of the world, I'm available to help.
+            If you&apos;re looking for a developer who can ensure your product is
+            effective in a large part of the world, I&apos;m available to help.
           </p>
         </>
       </Section>
