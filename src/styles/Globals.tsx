7988b66dd76464500eff0be37593f59d1677d017
Diogo <diogozang2@gmail.com>
2023-05-22 23:12:13 -0300

    feat:

--- src/styles/Globals.tsx
+++ src/styles/Globals.tsx
@@ -2,101 +2,90 @@ import Head from "next/head";
 
 export default function GlobalStyles() {
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap"
-          rel="stylesheet"
-        ></link>
-      </Head>
-      <style global jsx>{`
-        @import "./reset.css";
-        @import "./colors.css";
-        @import url("https://fonts.googleapis.com/css2?family=Sanchez&display=swap");
+    <style global jsx>{`
+      @import "./reset.css";
+      @import "./colors.css";
 
-        :root {
-          font-family: "Sanchez", monospace, sans-serif;
-        }
+      :root {
+        font-family: "Sanchez", monospace, sans-serif;
+      }
 
-        body {
-          background-color: var(--layer-000);
-          color: #eee;
-        }
+      body {
+        background-color: var(--layer-000);
+        color: #eee;
+      }
 
-        ul {
-          list-style-type: none;
-        }
+      ul {
+        list-style-type: none;
+      }
 
-        ol {
-          padding-left: 1rem;
-        }
+      ol {
+        padding-left: 1rem;
+      }
 
-        .stripe {
-          height: 1rem;
-        }
+      .stripe {
+        height: 1rem;
+      }
 
-        .center {
-          max-width: 1000px;
-          padding: 1rem;
-          margin: 0 auto;
-        }
+      .center {
+        max-width: 1000px;
+        padding: 1rem;
+        margin: 0 auto;
+      }
 
-        .gradient-bg {
-          background: linear-gradient(to right, var(--accent), var(--accent-2));
-        }
+      .gradient-bg {
+        background: linear-gradient(to right, var(--accent), var(--accent-2));
+      }
 
-        .gradient-text {
-          background: linear-gradient(to right, var(--accent), var(--accent-2));
-          width: fit-content;
-          color: transparent;
-          background-clip: text;
-        }
+      .gradient-text {
+        background: linear-gradient(to right, var(--accent), var(--accent-2));
+        width: fit-content;
+        color: transparent;
+        background-clip: text;
+      }
 
-        p {
-          font-size: 16px;
-          max-width: 70ch;
-          text-indent: 2ch;
-          margin-bottom: 1rem;
-          line-height: 1.8;
-        }
+      p {
+        font-size: 16px;
+        max-width: 70ch;
+        text-indent: 2ch;
+        margin-bottom: 1rem;
+        line-height: 1.8;
+      }
 
-        article h1 {
-          margin-bottom: 2rem;
-        }
+      article h1 {
+        margin-bottom: 2rem;
+      }
 
-        .accent {
-          color: var(--accent);
-        }
+      .accent {
+        color: var(--accent);
+      }
 
-        .bold {
-          font-weight: bold;
-          font-size: 18px;
-          line-height: 1.5;
-        }
+      .bold {
+        font-weight: bold;
+        font-size: 18px;
+        line-height: 1.5;
+      }
 
-        pre {
-          background-color: #000;
-          padding: 1rem;
-          border-radius: 8px;
-          max-width: 80ch;
-          margin: 1rem 0;
-        }
+      pre {
+        background-color: #000;
+        padding: 1rem;
+        border-radius: 8px;
+        max-width: 80ch;
+        margin: 1rem 0;
+      }
 
-        ::-webkit-scrollbar {
-          width: 8px;
-        }
+      ::-webkit-scrollbar {
+        width: 8px;
+      }
 
-        ::-webkit-scrollbar-track {
-          background: var(--layer-000);
-        }
+      ::-webkit-scrollbar-track {
+        background: var(--layer-000);
+      }
 
-        ::-webkit-scrollbar-thumb {
-          background: var(--accent);
-          border-radius: 8px;
-        }
-      `}</style>
-    </>
+      ::-webkit-scrollbar-thumb {
+        background: var(--accent);
+        border-radius: 8px;
+      }
+    `}</style>
   );
 }
